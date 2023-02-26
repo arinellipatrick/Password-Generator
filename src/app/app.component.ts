addd26d440bf8cb64ba7be8c7fc04963972bd7fb
arinellipatrick <arinellidev@gmail.com>
2023-02-26 09:46:27 -0300

    Password-Generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,58 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw-grider';
+  length = 0;
+  includeSymbols = false;
+  includeNumbers = false;
+  includeLetters = false;
+  password = '';
+
+  onChangeLength(event: any) {
+    const parsedValue = parseInt(event);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+    this.length = event.target.value;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%ˆ&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
